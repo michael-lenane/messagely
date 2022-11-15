a5d381d9dc8d8ae64c7ded03533eca6d3e7dcc31
Michael Lenane <michael.j.lenane@gmail.com>
2022-11-14 21:22:42 -0500

    add user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
+const Router = require("express").Router;
+const router = new Router();
+const User = require("../models/user");
+const Message = require("../models/message");
+const ExpressError = require("../expressError");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get("/", async function (res, req, next) {
+  try {
+    let users = await User.all();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /:username - get detail of users.
  *
@@ -11,6 +25,15 @@
  *
  **/
 
+router.get("/:username", async function (req, res, next) {
+  try {
+    const { username } = req.params.username;
+    let user = await User.get(username);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /:username/to - get messages to user
  *
@@ -22,7 +45,15 @@
  *
  **/
 
-
+router.get("/:username/to", async function (req, res, next) {
+  try {
+    const { username } = req.params.username;
+    let messages = await User.messagesTo(username);
+    return res.json({ messages });
+  } catch (err) {
+    return next(err);
+  }
+});
 /** GET /:username/from - get messages from user
  *
  * => {messages: [{id,
@@ -32,3 +63,13 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/from", async function (req, res, next) {
+  try {
+    const { username } = req.params.username;
+    let messages = await User.messagesFrom(username);
+    return res.json({ messages });
+  } catch (err) {
+    return next(err);
+  }
+});
